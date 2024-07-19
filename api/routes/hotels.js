29e11d8a2798a8734aebc441e9fd998739743014
nguyenjh <122562684+nguyenjh@users.noreply.github.com>
2024-07-18 21:49:39 -0700

    Created new CRUD operation for hotel.js and grabs an individual hotel's room(s)'s information based hotel's id

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
-import { countByCity, countByType, createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getAllHotel, getHotel, getHotelRooms, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -22,5 +22,6 @@ router.get("/find/:id", getHotel)
 router.get("/", getAllHotel)
 router.get("/countByCity", countByCity)
 router.get("/countByType", countByType)
+router.get("/room/:id", getHotelRooms)
 
 export default router;
